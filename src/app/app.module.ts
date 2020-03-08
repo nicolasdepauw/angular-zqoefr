76816913569205d2d7579765ee13a5e3de4f04e0
nicolasdepauw <nicolas.depauw@telenet.be>
2020-03-08 09:19:57 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
