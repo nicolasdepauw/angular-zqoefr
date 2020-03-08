078248c530ef991ad36a6c44de0bf5fa84b55fd7
nicolasdepauw <nicolas.depauw@telenet.be>
2020-03-08 12:43:48 +0100

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CounterService } from './counter.service';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  providers:    [ CounterService  ],
+  providers:    [ CounterService ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
