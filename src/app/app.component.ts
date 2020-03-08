e10acc2e16bd67b38561b1e18767884a92e213b4
nicolasdepauw <nicolas.depauw@telenet.be>
2020-03-08 12:36:23 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CounterService } from './counter.service';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'my-app',
