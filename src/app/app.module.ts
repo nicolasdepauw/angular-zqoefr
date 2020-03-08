bfdc1942d7c5b4d9dc468e879f31d5a586f3a989
nicolasdepauw <nicolas.depauw@telenet.be>
2020-03-08 19:40:08 +0100

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CounterService } from './counter.service';
-import { MaterialModule } from './material.module';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
